f8ef0fe9613a680873b1b1612e06b823b721ca6c
Albert Phu <127133346+albertphu07@users.noreply.github.com>
2026-07-18 23:07:18 +0000

    fix image issue

--- store/product.js
+++ store/product.js
@@ -1,40 +1,23 @@
 const images = [
-
-"../../images/IMG_7624.png",
-
-"../../images/IMG_7632(1).png",
-
-"../../images/IMG_7632(2).png",
-
-"../../images/IMG_7633.png",
-
-"../../images/IMG_7633(1).png",
-
+    "../../images/IMG_7624.png",
+    "../../images/IMG_7632(1).png",
+    "../../images/IMG_7632(2).png",
+    "../../images/IMG_7633.png",
+    "../../images/IMG_7633(1).png"
 ];
-
 
 let currentImage = 0;
 
-
-function changeImage(direction){
-
+function changeImage(direction) {
     currentImage += direction;
 
-
-    if(currentImage < 0){
-
+    if (currentImage < 0) {
         currentImage = images.length - 1;
-
     }
 
-
-    if(currentImage >= images.length){
-
+    if (currentImage >= images.length) {
         currentImage = 0;
-
     }
 
-
-    document.getElementById("product-image").src = images[currentImage];
-
+    document.querySelector("#product-image").setAttribute("src", images[currentImage]);
 }
